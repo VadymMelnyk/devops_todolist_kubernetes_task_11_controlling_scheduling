9e777e1d8773c3eb1722f3733a597e278a865c3e
Vadym <vadym@example.com>
2026-09-17 12:14:10 +0300

    add cluster.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+kind delete cluster
+kind create cluster --config cluster.yml
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
